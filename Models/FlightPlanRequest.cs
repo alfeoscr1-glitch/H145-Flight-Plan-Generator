b9f7cac6fa01b90713df4f4a9debe17410291f7d
alfeoscr1-glitch <alfeoscr1@gmail.com>
2026-09-03 18:24:52 +0100

    Add CoastlineMode and CoastlineLocation properties

--- Models/FlightPlanRequest.cs
+++ Models/FlightPlanRequest.cs
@@ -5,19 +5,14 @@ namespace H145FlightPlanner.Models
     public class FlightPlanRequest
     {
         public string Departure { get; set; } = string.Empty;
-
         public string Destination { get; set; } = string.Empty;
-
         public string RouteType { get; set; } = string.Empty;
-
         public string OrbitLocation { get; set; } = string.Empty;
-
+        public string CoastlineMode { get; set; } = string.Empty;
+        public string CoastlineLocation { get; set; } = string.Empty;
         public string ReturnLocation { get; set; } = string.Empty;
-
         public int? AltitudeFeet { get; set; }
-
         public string FlightRules { get; set; } = string.Empty;
-
         public List<string> RequestedLocations { get; set; } = new();
     }
 }
