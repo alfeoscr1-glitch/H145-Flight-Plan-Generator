ee4f3c51522b19005d99c80405ce37f8d5bf486c
alfeoscr1-glitch <alfeoscr1@gmail.com>
2026-09-03 14:33:36 +0100

    Add FlightPlanRequest model class

---
+++ Models/FlightPlanRequest.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace H145FlightPlanner.Models
+{
+    public class FlightPlanRequest
+    {
+        public string Departure { get; set; } = string.Empty;
+
+        public string Destination { get; set; } = string.Empty;
+
+        public string RouteType { get; set; } = string.Empty;
+
+        public string OrbitLocation { get; set; } = string.Empty;
+
+        public string ReturnLocation { get; set; } = string.Empty;
+
+        public int? AltitudeFeet { get; set; }
+
+        public string FlightRules { get; set; } = string.Empty;
+
+        public List<string> RequestedLocations { get; set; } = new();
+    }
+}
